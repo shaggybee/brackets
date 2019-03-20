3e63db0865e09777677512c6379af8f4556e25ae
shaggybee <kislov.v@gmail.com>
2019-03-20 14:13:50 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
+function checkBrackets(bracket, arrayBrackets){
+  let objBracket = {"bracket" : 'not found', "position" : 0};
+  for (let i = 0; i < arrayBrackets.length; i++){
+      if ((bracket === arrayBrackets[i][0]) & (bracket === arrayBrackets[i][1])){
+        objBracket["bracket"] = 'identic';
+        objBracket["position"] = i;
+        return objBracket;
+      }
+      if (bracket === arrayBrackets[i][0]){
+        objBracket["bracket"] = 'open';
+        objBracket["position"] = i;
+      } 
+      else if (bracket === arrayBrackets[i][1]){
+        objBracket["bracket"] = 'close';
+        objBracket["position"] = i;
+      } 
+  }
+  return objBracket;
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let objBracket = {"bracket" : 'not found', "position" : 0};
+  let stackBrackets = [];   
+  for (let i = 0; i < str.length; i++){
+
+    objBracket = checkBrackets(str[i], bracketsConfig);
+    
+    if (objBracket["bracket"] === 'identic'){
+      let Bracket = stackBrackets.pop();
+      if (Bracket === undefined) stackBrackets.push(str[i]);
+      else if (Bracket != str[i]) {
+        stackBrackets.push(Bracket);
+        stackBrackets.push(str[i]);
+      }
+      continue;
+    }
+
+    if (objBracket["bracket"] === 'open') {
+      stackBrackets.push(str[i]);
+      continue;
+    }
+    else if (objBracket["bracket"] === 'close') {
+      let openBracket = stackBrackets.pop();
+      if (openBracket === undefined) return false;
+      if (openBracket === bracketsConfig[objBracket["position"]][0]) continue;
+      else return false;
+    }
+    else return false;
+  }
+  
+  let a = stackBrackets.pop();
+
+  if (a === undefined) return true;
+  else return false;
 }
